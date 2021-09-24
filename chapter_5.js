ebd2c9c1395d22ed631bc78443817b67c29d0276
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-09-24 12:25:39 +0300

    Update chapter_5.js

--- chapter_5.js
+++ chapter_5.js
@@ -1,1 +1,108 @@
+// ==========================================================
+/* 
+A Vector Type
+Write a class Vec that represents a vector in two-dimensional space. It takes
+x and y parameters (numbers), which it should save to properties of the
+same name.
+Give the Vec prototype two methods, plus and minus, that take another
+vector as a parameter and return a new vector that has the sum or difference
+of the two vectors’ (this and the parameter) x and y values.
+Add a getter property length to the prototype that computes the length
+of the vector—that is, the distance of the point (x, y) from the origin (0, 0)
 
+ */
+
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vec) {
+    return new Vec(vec.x + this.x, vec.y + this.y);
+  }
+
+  minus(vec) {
+    return new Vec(this.x - vec.x, this.y - vec.y);
+  }
+
+  get length() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(3, 4).length);
+
+/* 
+Groups
+The standard JavaScript environment provides another data structure called
+Set. Like an instance of Map, a set holds a collection of values. Unlike Map,
+it does not associate other values with those—it just tracks which values
+are part of the set. A value can be part of a set only once—adding it again
+doesn’t have any effect.
+Write a class called Group (since Set is already taken). Like Set, it has add,
+delete, and has methods. Its constructor creates an empty group, add adds a
+value to the group (but only if it isn’t already a member), delete removes its
+argument from the group (if it was a member), and has returns a Boolean
+value indicating whether its argument is a member of the group.
+Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
+Give the class a static from method that takes an iterable object as argument and creates a group that contains all the values produced by iterating
+over it.
+
+ */
+
+class Group {
+  #array = [];
+
+  constructor(array) {
+    for (const value of array) {
+      this.add(value);
+    }
+  }
+
+  add(value) {
+    if (this.has(value)) {
+      return this;
+    } else {
+      this.#array.push(value);
+      return this;
+    }
+  }
+
+  has(value) {
+    let num = this.#array.indexOf(value);
+    return num === -1 ? false : true;
+  }
+
+  delete(value) {
+    let num = this.#array.indexOf(value);
+    if (num === -1) {
+      return this;
+    } else {
+      this.#array = this.#array.filter((_, index) => {
+        if (num !== index) {
+          return true;
+        }
+        return false;
+      });
+      return this;
+    }
+  }
+
+  static from(array) {
+    return new Group(array);
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+console.log(group.has(10));
+group.delete(10);
+console.log(group.has(10));
+// → false
