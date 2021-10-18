fd9b9f10fff676d89d3f1d97c2866097997e2e09
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-10-18 10:57:03 +0300

    Update CSX Challenge Solution.js

--- CSX Challenge Solution.js
+++ CSX Challenge Solution.js
@@ -19,5 +19,47 @@ obj = {
 };
 
 console.log(arrayBuilder(obj));
+//===============================================================================================
 
+// https://www.youtube.com/watch?v=ELX-FBXZ-3g&list=PLWrQZnG8l0E5qPeomg7pmAzQF4vTvLnzV&index=2
+// Reduce (CSX Callbacks and Higher Order Functions Unit)
+var nums = [4, 1, 3];
+var add = (a, b) => {
+  return a + b;
+};
 
+function reduce(array, func, beginValue = 0) {
+  for (const value of array) {
+    beginValue = func(beginValue, value);
+  }
+
+  return beginValue;
+}
+
+console.log(reduce(nums, add, 0));
+
+//===============================================================================================
+//https://www.youtube.com/watch?v=micF_oeeeko&list=PLWrQZnG8l0E5qPeomg7pmAzQF4vTvLnzV&index=3
+//runInOrder (CSX Async Unit)
+function runInOrder(functions, timeouts) {
+  for (const array of functions.entries()) {
+    setTimeout(array[1], timeouts[array[0]]);
+  }
+}
+
+function sayHi() {
+  console.log("hi");
+}
+
+function sayBye() {
+  console.log("bye");
+}
+
+function sayHowdy() {
+  console.log("howdy");
+}
+
+fnc = [sayHi, sayBye, sayHowdy];
+tm = [300, 600, 200];
+
+runInOrder(fnc, tm);
